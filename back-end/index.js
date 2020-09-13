a02dbce70ffe25874c9dd8f73ba39014d868fb2a
mpsalome <marcus.salome@restoque.com.br>
2020-09-13 17:23:37 -0300

    Folder renomeado pra monitorar as mudanças no folder

---
+++ back-end/index.js
@@ -0,0 +1,38 @@
+import express from "express"
+import winston from "winston"
+import tempRouter from "./routes/temperatura.js"
+import cors from "cors"
+
+
+// Definindo logger
+const { combine, timestamp, label, printf } = winston.format
+const myFormat = printf(({ level, message, label, timestamp }) => {
+    return `${timestamp} [${label}] ${level}: ${message}`
+})
+global.logger = winston.createLogger({
+    level: "silly",
+    transports: [
+        new (winston.transports.Console),
+        new (winston.transports.File)({ filename: "aquaberry-server.log" }),
+
+    ],
+    format: combine(
+        label({ label: "aquaberry-server" }),
+        timestamp(),
+        myFormat
+    )
+})
+
+// Configurando Server 
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use("/temperatura", tempRouter)
+
+app.listen(3010, async () => {
+    try {
+        logger.info("Servidor Iniciado")
+    } catch (error) {
+        logger.error(err);
+    }
+})
