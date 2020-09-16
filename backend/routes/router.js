0c55ec3ab794f5ed465e7b425680cc203e4a3ee9
mpsalome <marcuspedro7@gmail.com>
2020-09-15 22:33:51 -0300

    Relés configurados

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,14 +1,32 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
 
 import express from "express"
-import ds18x20 from "ds18x20";
-const wsserver = require("ws").Server;
-const wss = new wsserver({ port : 3011 });
+import ds18x20 from "ds18x20"
+const wsserver = require("ws").Server
+const wss = new wsserver({ port : 3011 })
+const gpio = require('onoff').Gpio
 
 const sensor = ds18x20
 
 const router = express.Router()
+
+const relays = [
+    new gpio(14, 'out'), // rele 2
+    new gpio(15, 'out'), // rele 1
+    new gpio(18, 'out')  // rele 3
+  ]
+
+// Função pra desligart todos os rele 
+const allRelaysOff = () => {
+    console.log("desligando reles")
+    relays.forEach(relay => {
+        relay.readSync()
+        console.log("desligando esse rele")
+        relay.writeSync(1)
+        relay.readSync()
+    })
+ }
 
 // Carregando o sensor
 try {
@@ -33,7 +51,7 @@ console.log(listOfDeviceIds)
 // Rotas
 router.get("/", async (req, res, next) => {
     try {
-        const data = sensor.get('28-3c01b5567b40');
+        const data = sensor.get('28-3c01b5567b40')
         console.log(data)
         res.send(data.toString())
         logger.info(`GET /temperatura`)
@@ -44,21 +62,44 @@ router.get("/", async (req, res, next) => {
 
 // Websocket 
 wss.on("connection", function(ws) {
-    var timer = setInterval(enviarTemperatura, 1000, ws);  
+    var timer = setInterval(lerTemperatura, 1000, ws)  
     
     ws.on("message", function(message) {
-        console.log(message);
-    }); 
+        console.log(message)
+    }) 
     
     ws.on("close", function(client) {
-        clearInterval(timer);
-    });
-});
+        clearInterval(timer)
+    })
+})
 
-function enviarTemperatura(socket) {
-    let temp = sensor.get('28-3c01b5567b40');
-    socket.send(temp);
+const lerTemperatura = socket => {
+    let temp = sensor.get('28-3c01b5567b40')
+    socket.send(temp)
+    handleTemperatura(temp)
 }
 
+// Ligar Relé baseado na temperatura
+const handleTemperatura = temp => {
+    if (Number(temp) > 24) {
+        if (relays[0].readSync() == 1) {
+            relays[0].writeSync(0)
+            logger.info("ligando Cooler")
+        } 
+    } else if (Number(temp) < 24 && Number(temp) < 24) {
+        if (relays[0].readSync() == 0 || relays[1].readSync() == 0 ) {
+            relays[0].writeSync(1)
+            relays[1].writeSync(1)
+        }
+    } else {
+        if (relays[1].readSync() == 1) {
+            relays[0].writeSync(0)
+            logger.info("ligando aquecedor")
+        } 
+    }
+}
+
+// Desligando todos os reles no start up.
+allRelaysOff()
 
 export default router
