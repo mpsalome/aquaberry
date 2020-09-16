cc27f8188729c29443d95b00a35ef6782f62128d
mpsalome <marcuspedro7@gmail.com>
2020-09-16 09:45:52 -0300

    Refatoração de código

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -12,91 +12,86 @@ const sensor = ds18x20
 const router = express.Router()
 
 const relays = [
-    new gpio(14, 'out'), // rele 2
     new gpio(15, 'out'), // rele 1
+    new gpio(14, 'out'), // rele 2
     new gpio(18, 'out')  // rele 3
   ]
 
-// Função pra desligart todos os rele 
-const allRelaysOff = () => {
-    console.log("desligando reles")
-    relays.forEach(relay => {
-        relay.readSync()
-        console.log("desligando esse rele")
-        relay.writeSync(1)
-        relay.readSync()
-    })
- }
 
-// Carregando o sensor
+
+// Carregando o sensor de temperatura 
 try {
     sensor.loadDriver()
-    console.log('driver is loaded')
+    console.log('Driver carregado com sucesso!')
+    logger.info(`Driver carregado com sucesso!`)
 } catch (err) {
-    console.log('something went wrong loading the driver:', err)
+    console.log('Erro ao carregar o driver:', err)
+    logger.info(`Erro ao carregar o driver: ${err}`)
 }
 
-
-// Checando se o sensor está conectado 
-var isLoaded = sensor.isDriverLoaded()
-console.log('isLoaded')
-console.log(isLoaded)
-
-// Lista de Sensores conectados
-var listOfDeviceIds = sensor.list()
-console.log('listOfDeviceIds')
-console.log(listOfDeviceIds)
-
-
 // Rotas
-router.get("/", async (req, res, next) => {
+router.get("/temperatura", async (req, res, next) => {
     try {
-        const data = sensor.get('28-3c01b5567b40')
-        console.log(data)
+        let data = sensor.get('28-3c01b5567b40')
+        console.log(`Leitura de temperatura requisitada pela rota: ${data}`)
         res.send(data.toString())
-        logger.info(`GET /temperatura`)
+        logger.info(`GET /temperatura: ${data.toString()}`)
     } catch (err) {
+        logger.info(`Erro ao pegar a temperatura: ${err}`)
         next(err)
     }
 })
 
 // Websocket 
-wss.on("connection", function(ws) {
+wss.on("connection", ws => {
     var timer = setInterval(lerTemperatura, 1000, ws)  
     
-    ws.on("message", function(message) {
+    ws.on("message", message => {
         console.log(message)
     }) 
     
-    ws.on("close", function(client) {
+    ws.on("close", () => {
         clearInterval(timer)
     })
 })
 
-const lerTemperatura = socket => {
-    let temp = sensor.get('28-3c01b5567b40')
-    socket.send(temp)
-    handleTemperatura(temp)
-}
+// Funções
+
+// Função pra desligart todos os rele 
+const allRelaysOff = () => {
+    relays.forEach((relay, index) => {
+        console.log(`Status do rele ${index+1}`)
+        console.log(`Desligando o rele ${index+1}`)
+        relay.writeSync(1)
+    })
+ }
 
 // Ligar Relé baseado na temperatura
 const handleTemperatura = temp => {
     if (Number(temp) > 24) {
         if (relays[0].readSync() == 1) {
             relays[0].writeSync(0)
-            logger.info("ligando Cooler")
+            logger.info("Ligando Cooler")
         } 
     } else if (Number(temp) < 24 && Number(temp) < 24) {
         if (relays[0].readSync() == 0 || relays[1].readSync() == 0 ) {
+            logger.info("Temperatura ideal atingida. Desligando aquecedor e/ou Cooler")
             relays[0].writeSync(1)
             relays[1].writeSync(1)
         }
     } else {
         if (relays[1].readSync() == 1) {
             relays[0].writeSync(0)
-            logger.info("ligando aquecedor")
+            logger.info("Ligando aquecedor")
         } 
     }
+}
+
+//Ler temperatura do sensor
+const lerTemperatura = socket => {
+    let temp = sensor.get('28-3c01b5567b40')
+    socket.send(temp)
+    handleTemperatura(temp)
 }
 
 // Desligando todos os reles no start up.
