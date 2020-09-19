4e9969bdeaa7bcefdfbe65ad9b1f7a44268975d7
mpsalome <marcuspedro7@gmail.com>
2020-09-19 17:17:48 -0300

    Criada tela de status

---
+++ frontend/src/components/Status.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="box__status row">
+    <div class="col-12 col-md-6">
+      Cooler: <span v-if="cooler">ligado</span><span v-else>desligado</span>
+    </div>
+    <div class="col-12 col-md-6">
+      Filtro de Água: <span v-if="filtro">ligado</span
+      ><span v-else>desligado</span>
+    </div>
+    <div class="col-12 col-md-6">
+      LEDs: <span v-if="led">ligado</span><span v-else>desligado</span>
+    </div>
+    <div class="col-12 col-md-6">
+      Aquecedor: <span v-if="aquecedor">ligado</span
+      ><span v-else>desligado</span>
+    </div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+import API from "@/services/api";
+
+export default {
+  name: "Chart",
+  props: {},
+  data: function() {
+    return {
+      cooler: true,
+      filtro: true,
+      led: true,
+      aquecedor: true
+    };
+  },
+  created: function() {
+  },
+  methods: {
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.box__status {
+    margin-top: 15px;
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="home">
+    <div class="row">
+      <div class="col">
+        <h1 class="title">Bem-vindo ao Aquaberry</h1>
+      </div>
+    </div>
+    <Status />
     <Chart />
   </div>
 </template>
 
 <script>
 import Chart from "@/components/Chart.vue";
+import Status from "@/components/Status.vue";
 export default {
   name: "Home",
   components: {
-    Chart
+    Chart,
+    Status
   }
 };
 </script>
