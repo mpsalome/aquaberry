3c27100fe0bc16f09e52b40581c11a87d7d6ccb9
mpsalome <marcuspedro7@gmail.com>
2020-09-19 17:43:36 -0300

    Consumo de api com axios

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,17 +1,48 @@
+const axios = require("axios");
+
 function get(theUrl) {
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open("GET", theUrl, false);
-  xmlHttp.send(null);
-  return xmlHttp.responseText;
+  axios
+    .get(theUrl)
+    .then(function(response) {
+      return response;
+    })
+    .catch(function(error) {
+      return error;
+    });
 }
+
+function post(theUrl, body) {
+  axios
+    .post(theUrl, body)
+    .then(function(response) {
+      return response;
+    })
+    .catch(function(error) {
+      return error;
+    });
+}
+
+const IP = "http://192.168.15.15:3010";
 
 export default {
   getTemperatura() {
-    var value = get("http://192.168.15.48:3010/temperatura");
+    var value = get(`${IP}/temperatura`);
     return value;
   },
   getPh() {
-    var value = get("http://192.168.15.48:3010/ph");
+    var value = get(`${IP}/ph`);
+    return value;
+  },
+  getStatusReles() {
+    var value = get(`${IP}/statusReles`);
+    return value;
+  },
+  postReleOff() {
+    var value = post(`${IP}/releOff`);
+    return value;
+  },
+  postReleOn() {
+    var value = post(`${IP}/releOn`);
     return value;
   }
 };
