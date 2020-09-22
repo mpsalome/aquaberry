cc40cfd2919552bff1cc7030b80c75bef71a9680
mpsalome <marcuspedro7@gmail.com>
2020-09-22 20:37:41 -0300

    Pacote de ícones adicionado

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,11 +8,15 @@ import 'buefy/dist/buefy.css'
 import VueSidebarMenu from "vue-sidebar-menu";
 import "vue-sidebar-menu/dist/vue-sidebar-menu.css";
 import VueSvgGauge from 'vue-svg-gauge'
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/css/fontawesome.css";
 
 Vue.use(VueSidebarMenu);
 Vue.use(Buefy);
+Vue.use(Buefy, {
+  defaultIconPack: 'fas'
+});
 Vue.use(VueSvgGauge)
-
 
 Vue.config.productionTip = false;
 
