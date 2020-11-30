b907dd88a7bf1646aa615fb898142905e3156800
mpsalome <marcuspedro7@gmail.com>
2020-11-30 19:17:03 -0300

    Tooltip nos campos

--- frontend/src/views/Settings.vue
+++ frontend/src/views/Settings.vue
@@ -3,126 +3,137 @@
     <h2 class="title">Configurações</h2>
 
     <h3 class="subtitle">Controle de Temperatura</h3>
-    <b-field>
-      <b-input
-        placeholder="Temperatura Máxima"
-        type="number"
-        icon-pack="fas"
-        icon="thermometer-full"
-        v-model="maxTemp"
-        :disabled="maxTemp !== '' ? true : false"
-        class="maxTemp"
-      ></b-input>
-      <b-icon
-        pack="fa"
-        class="fakeButton maxTemp"
-        icon="edit"
-        size="is-small"
-        @click.native="editInfo"
-      >
-      </b-icon>
-    </b-field>
-    <b-field>
-      <b-input
-        placeholder="Temperatura Ideal"
-        type="number"
-        icon-pack="fas"
-        icon="thermometer-half"
-        v-model="idealTemp"
-        :disabled="idealTemp !== '' ? true : false"
-        class="idealTemp"
-      ></b-input>
-      <b-icon
-        pack="fa"
-        class="fakeButton idealTemp"
-        icon="edit"
-        size="is-small"
-        @click.native="editInfo"
-      >
-      </b-icon>
-    </b-field>
-    <b-field>
-      <b-input
-        placeholder="Temperatura Mínima"
-        type="number"
-        icon-pack="fas"
-        icon="thermometer-empty"
-        v-model="minTemp"
-        :disabled="minTemp !== '' ? true : false"
-        class="minTemp"
-      ></b-input>
-      <b-icon
-        pack="fa"
-        class="fakeButton minTemp"
-        icon="edit"
-        size="is-small"
-        @click.native="editInfo"
-      >
-      </b-icon>
-    </b-field>
-
+    <b-tooltip label="Temperatura Máxima do aquário">
+      <b-field>
+        <b-input
+          placeholder="Temperatura Máxima"
+          type="number"
+          icon-pack="fas"
+          icon="thermometer-full"
+          v-model="maxTemp"
+          :disabled="maxTemp !== '' ? true : false"
+          class="maxTemp"
+        ></b-input>
+        <b-icon
+          pack="fa"
+          class="fakeButton maxTemp"
+          icon="edit"
+          size="is-small"
+          @click.native="editInfo"
+        >
+        </b-icon>
+      </b-field>
+    </b-tooltip>
+    <b-tooltip label="Temperatura Ideal do aquário">
+      <b-field>
+        <b-input
+          placeholder="Temperatura Ideal"
+          type="number"
+          icon-pack="fas"
+          icon="thermometer-half"
+          v-model="idealTemp"
+          :disabled="idealTemp !== '' ? true : false"
+          class="idealTemp"
+        ></b-input>
+        <b-icon
+          pack="fa"
+          class="fakeButton idealTemp"
+          icon="edit"
+          size="is-small"
+          @click.native="editInfo"
+        >
+        </b-icon>
+      </b-field>
+    </b-tooltip>
+    <b-tooltip label="Temperatura Mínima do aquário">
+      <b-field>
+        <b-input
+          placeholder="Temperatura Mínima"
+          type="number"
+          icon-pack="fas"
+          icon="thermometer-empty"
+          v-model="minTemp"
+          :disabled="minTemp !== '' ? true : false"
+          class="minTemp"
+        ></b-input>
+        <b-icon
+          pack="fa"
+          class="fakeButton minTemp"
+          icon="edit"
+          size="is-small"
+          @click.native="editInfo"
+        >
+        </b-icon>
+      </b-field>
+    </b-tooltip>
     <h3 class="subtitle">Controle de Alimentação</h3>
-    <b-field v-for="(hora, index) in horaAlimentacao" :key="index">
-      <b-input
-        placeholder="Horário de alimentação do peixe"
-        icon-pack="far"
-        icon="clock"
-        v-model="hora.value"
-        :disabled="hora.value.length < 8 ? false : true"
-        class="deleteTimer newTimer"
-        v-cleave="masks.time"
-      ></b-input>
-      <b-icon
-        pack="fa"
-        class="fakeButton deleteTimer"
-        icon="trash"
-        size="is-small"
-        @click.native="editInfo"
-      >
-      </b-icon>
-    </b-field>
+    <b-tooltip label="Horário de Alimentação">
+      <b-field v-for="(hora, index) in horaAlimentacao" :key="index">
+        <b-input
+          placeholder="Horário de alimentação do peixe"
+          icon-pack="far"
+          icon="clock"
+          v-model="hora.value"
+          :disabled="hora.value.length < 8 ? false : true"
+          class="deleteTimer newTimer"
+          v-cleave="masks.time"
+        ></b-input>
+        <b-icon
+          pack="fa"
+          class="fakeButton deleteTimer"
+          icon="trash"
+          size="is-small"
+          @click.native="editInfo"
+        >
+        </b-icon>
+      </b-field>
+    </b-tooltip>
     <b-button type="is-info" icon-left="plus" class="newTimer" @click="addHora"
       >Adicionar horário</b-button
     >
     <h3 class="subtitle">Controle da Iluminação</h3>
-    <b-field>
-      <b-input
-        placeholder="Hora Inicio"
-        icon-pack="far"
-        icon="sun"
-        v-model="iniLuz"
-        :disabled="iniLuz !== '' ? true : false"
-        class="startTime"
-        v-cleave="masks.time"
-      ></b-input>
-      <b-icon
-        pack="fa"
-        class="fakeButton startTime"
-        icon="edit"
-        size="is-small"
-        @click.native="editInfo"
-      >
-      </b-icon>
-    </b-field>
-    <b-field>
-      <b-input
-        placeholder="Hora Fim"
-        icon-pack="far"
-        icon="moon"
-        v-model="fimLuz"
-        :disabled="fimLuz !== '' ? true : false"
-        class="endTime"
-        v-cleave="masks.time"
-      ></b-input>
-      <b-icon
-        pack="fa"
-        class="fakeButton endTime"
-        icon="edit"
-        size="is-small"
-        @click.native="editInfo"
-      >
-      </b-icon>
-    </b-field>
+    <b-tooltip label="Hora de ínicio do ciclo de luz">
+      <b-field>
+        <b-input
+          placeholder="Hora Inicio"
+          icon-pack="far"
+          icon="sun"
+          v-model="iniLuz"
+          :disabled="iniLuz !== '' ? true : false"
+          class="startTime"
+          v-cleave="masks.time"
+        ></b-input>
+        <b-icon
+          pack="fa"
+          class="fakeButton startTime"
+          icon="edit"
+          size="is-small"
+          @click.native="editInfo"
+        >
+        </b-icon>
+      </b-field>
+    </b-tooltip>
+    <b-tooltip label="Hora de fim do ciclo de luz">
+      <b-field>
+        <b-input
+          placeholder="Hora Fim"
+          icon-pack="far"
+          icon="moon"
+          v-model="fimLuz"
+          :disabled="fimLuz !== '' ? true : false"
+          class="endTime"
+          v-cleave="masks.time"
+        ></b-input>
+        <b-icon
+          pack="fa"
+          class="fakeButton endTime"
+          icon="edit"
+          size="is-small"
+          @click.native="editInfo"
+        >
+        </b-icon>
+      </b-field>
+    </b-tooltip>
     <div class="buttons">
       <b-button
         type="is-dark"
@@ -348,5 +359,9 @@ export default {
     top: 31%;
     left: 10px;
     cursor: pointer;
+  }
+  span.b-tooltip {
+    width: 100%;
+    margin-bottom: 20px;
   }
 </style>
